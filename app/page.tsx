6cbf9f3153c799a7330df9ab678d543e2bbf9237
bijay91lt <bijay3990@gmail.com>
2025-10-29 17:34:56 +0545

    sticky-scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/ui/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/96 antialiased bg-grid-white/2">
      <HeroSection />
      <FeaturedCourses />
+     <WhyChooseUs/>
     </main>
   );
 }
