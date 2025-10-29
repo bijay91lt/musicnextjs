93db737ac7acb462d96ad315bf44767961d5b963
bijay91lt <bijay3990@gmail.com>
2025-10-29 16:18:59 +0545

    Featured Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/96 antialiased bg-grid-white/2">
-     <HeroSection></HeroSection>
+     <HeroSection />
+     <FeaturedCourses />
     </main>
   );
 }
